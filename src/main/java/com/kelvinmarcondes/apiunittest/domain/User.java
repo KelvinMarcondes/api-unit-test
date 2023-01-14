5e2a2698a5153fd1176b4f183299ea71d7233ad6
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-14 20:10:31 -0300

    Interface UserRepository

--- src/main/java/com/kelvinmarcondes/apiunittest/domain/User.java
+++ src/main/java/com/kelvinmarcondes/apiunittest/domain/User.java
@@ -1,19 +1,18 @@
 package com.kelvinmarcondes.apiunittest.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
     private String name;
 
     @Column(unique = true)
