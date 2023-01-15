46efdea3a36c1f4ec3acc72096cb47b090ab1e1a
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-14 23:11:41 -0300

    endpoint findAll para user

--- src/main/java/com/kelvinmarcondes/apiunittest/services/UserService.java
+++ src/main/java/com/kelvinmarcondes/apiunittest/services/UserService.java
@@ -2,7 +2,11 @@ package com.kelvinmarcondes.apiunittest.services;
 
 import com.kelvinmarcondes.apiunittest.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
