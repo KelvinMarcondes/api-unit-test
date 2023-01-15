59a1322c6f231031693702dc2a94fbda7429b318
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-14 22:03:33 -0300

    Disponibilizando modelmapper em forma de Bean

---
+++ src/main/java/com/kelvinmarcondes/apiunittest/config/ModelMapperConfig.java
@@ -0,0 +1,15 @@
+package com.kelvinmarcondes.apiunittest.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
