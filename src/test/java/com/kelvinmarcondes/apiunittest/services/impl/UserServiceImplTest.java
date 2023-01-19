4640d74cf458905b0975d5904f182fcd4e4cd6fd
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-19 13:46:24 -0300

    Teste para classe UserService metodo update com sucesso

--- src/test/java/com/kelvinmarcondes/apiunittest/services/impl/UserServiceImplTest.java
+++ src/test/java/com/kelvinmarcondes/apiunittest/services/impl/UserServiceImplTest.java
@@ -122,8 +122,24 @@ class UserServiceImplTest {
             assertEquals(DataIntegrityViolationException.class, ex.getClass());
             assertEquals("Email já cadastrado no sistema.", ex.getMessage());
         }
-
     }
+
+    @Test
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+    }
+
+
+
 
     @Test
     void update() {
