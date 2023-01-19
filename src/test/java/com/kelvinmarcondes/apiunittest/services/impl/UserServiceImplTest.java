0921f4b43d87ac3cb3365e2ebd073be6068e1ac3
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-01-19 13:52:07 -0300

    Teste para classe UserService metodo update com excecao

--- src/test/java/com/kelvinmarcondes/apiunittest/services/impl/UserServiceImplTest.java
+++ src/test/java/com/kelvinmarcondes/apiunittest/services/impl/UserServiceImplTest.java
@@ -30,6 +30,7 @@ class UserServiceImplTest {
     public static final String PASSWORD = "123";
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
     public static final int INDEX = 0;
+    public static final String EMAIL_JA_CADASTRADO_NO_SISTEMA = "Email já cadastrado no sistema.";
 
     @InjectMocks
     private UserServiceImpl service;
@@ -120,7 +121,7 @@ class UserServiceImplTest {
             service.create(userDTO);
         }catch(Exception ex){
             assertEquals(DataIntegrityViolationException.class, ex.getClass());
-            assertEquals("Email já cadastrado no sistema.", ex.getMessage());
+            assertEquals(EMAIL_JA_CADASTRADO_NO_SISTEMA, ex.getMessage());
         }
     }
 
@@ -138,7 +139,18 @@ class UserServiceImplTest {
         assertEquals(PASSWORD, response.getPassword());
     }
 
+    @Test
+    void whenUpdateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
 
+        try{
+            optionalUser.get().setId(2);
+            service.update(userDTO);
+        }catch(Exception ex){
+            assertEquals(DataIntegrityViolationException.class, ex.getClass());
+            assertEquals(EMAIL_JA_CADASTRADO_NO_SISTEMA, ex.getMessage());
+        }
+    }
 
 
     @Test
